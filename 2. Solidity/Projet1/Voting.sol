fc95054991b28b7b322a97590621c8a8074159c2
Compote0 <pierreorgeret@gmail.com>
2024-02-07 16:52:07 +0100

    :sparkles: :feat: init Projet1 Voting contract

---
+++ 2. Solidity/Projet1/Voting.sol
@@ -0,0 +1,78 @@
+/* 
+- L’administrateur qui déploie le smart contract est celui qui gère le processus de vote.
+
+- Import “Ownable” d’OpenZeppelin pour gérer les droits d’administration.
+
+- Définir les structures de données suivantes :
+Voter : Contient des informations sur chaque électeur (inscrit, a voté, ID de la proposition votée).
+Proposal : Contient la description d’une proposition et le nombre de votes reçus.
+
+- Utiliser une énumération (WorkflowStatus) pour gérer les différents états du vote :
+Enregistrement des électeurs.
+Début de l’enregistrement des propositions.
+Fin de l’enregistrement des propositions.
+Début de la session de vote.
+Fin de la session de vote.
+Votes comptabilisés.
+
+- Définir un uint winningProposalId pour représenter l’ID de la proposition gagnante.
+/ créer une func getWinner qui retourne l’ID du gagnant.
+
+- Déclarer les événements suivants :
+VoterRegistered : Pour signaler l’inscription d’un électeur.
+WorkflowStatusChange : Pour indiquer le changement d’état du vote.
+ProposalRegistered : Pour enregistrer une nouvelle proposition.
+Voted : Pour enregistrer un vote.
+
+*/
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    WorkflowStatus public currentStatus;
+    uint public winningProposalId;
+
+    mapping(address => Voter) public voters;
+    Proposal[] public proposals;
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted(address voter, uint proposalId);
+
+
+    constructor(address initialOwner) Ownable(initialOwner) {
+        currentStatus = WorkflowStatus.RegisteringVoters;
+    }
+
+    modifier onlyInStatus(WorkflowStatus _status) {
+        require(currentStatus == _status, "Invalid workflow status");
+        _;
+    }
+
+}
+
