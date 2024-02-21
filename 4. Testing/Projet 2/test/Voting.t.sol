014ae82037a309a6e8726fe8402efd8cf8deabc1
Compote0 <pierreorgeret@gmail.com>
2024-02-21 18:03:16 +0100

    :white_check_mark: adding

--- 4. Testing/Projet 2/test/Voting.t.sol
+++ 4. Testing/Projet 2/test/Voting.t.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/votingPlus.sol";
+import "../src/Voting.sol";
 
 contract VotingTest is Test {
 
- Voting voting;
+    Voting voting;
 
     function setUp() public {
         voting = new Voting();
+    }
+
+    function test_InitialWorkflowStatusIsRegisteringVoters() public {
+        Voting.WorkflowStatus initialStatus = voting.workflowStatus();
+        assertEq(uint(initialStatus), uint(Voting.WorkflowStatus.RegisteringVoters), "Initial workflow status should be RegisteringVoters");
     }
 
 }
